d3c097febae3b3ef89412fd5f7860c086cba2cdb
Plexiate <brian@plexiate.com>
2023-05-07 18:29:31 -0400

    Fixed a permissions issue.
    Fixed a permissions issue.
    
    You can now use the command /pdc reload with "playerdeathcoords.reload"

--- src/main/java/one/crafters/playerdeathcoords/commands/CommandManagerClass.java
+++ src/main/java/one/crafters/playerdeathcoords/commands/CommandManagerClass.java
@@ -1,6 +1,7 @@
 package one.crafters.playerdeathcoords.commands;
 
 import one.crafters.playerdeathcoords.commands.subcommands.ReloadCommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +9,9 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public class CommandManagerClass implements CommandExecutor {
 
@@ -21,20 +25,26 @@ public class CommandManagerClass implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
+
         if (sender instanceof Player){
             Player p = (Player) sender;
 
-            if (args.length > 0){
-                for (int i = 0; i < getSubcommands().size(); i++){
-                    if (args[0].equalsIgnoreCase(getSubcommands().get(i).getName())){
-                        getSubcommands().get(i).perform(p, args);
+            if (sender.hasPermission("playerdeathcoords.reload")){
+
+                if (args.length > 0){
+                    for (int i = 0; i < getSubcommands().size(); i++){
+                        if (args[0].equalsIgnoreCase(getSubcommands().get(i).getName())){
+                            getSubcommands().get(i).perform(p, args);
+                        }
                     }
                 }
+
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fUnknown command. Type ''/help'' for help."));
             }
-
+        } else {
+            getLogger().info("You need to do this command in game.");
         }
-
-
 
         return true;
     }
